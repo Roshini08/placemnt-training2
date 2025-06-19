0cde08fc4059f3d1735d4682caeb12ed201aad0b
Roshini08 <151143452+Roshini08@users.noreply.github.com>
2025-06-19 19:22:33 +0530

    Create ques14.java

---
+++ ques14.java
@@ -0,0 +1,29 @@
+public class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int k = 1; 
+        for (int i = 1; i < nums.length; i++) {
+           
+            if (nums[i] != nums[k - 1]) {
+                nums[k] = nums[i]; 
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicates obj = new RemoveDuplicates();
+
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int k = obj.removeDuplicates(nums);
+
+        System.out.println("k = " + k);
+        System.out.print("Updated array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
