13a6955f6ded32fbea89d3ffe8774fb9998eac52
Roshini08 <151143452+Roshini08@users.noreply.github.com>
2025-06-19 19:23:37 +0530

    Create ques15.java

---
+++ ques15.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+   
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);    map.put('V', 5);
+        map.put('X', 10);   map.put('L', 50);
+        map.put('C', 100);  map.put('D', 500);
+        map.put('M', 1000);
+
+        int total = 0;
+        int prev = 0;
+
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int curr = map.get(s.charAt(i));
+
+         
+            if (curr < prev) {
+                total -= curr;
+            } else {
+                total += curr;
+            }
+
+            prev = curr;
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+        RomanToInteger converter = new RomanToInteger();
+        System.out.println(converter.romanToInt("III"));     /
+        System.out.println(converter.romanToInt("LVIII"));    
+        System.out.println(converter.romanToInt("MCMXCIV")); 
+    }
+}
