9a8b94ddd04551ac2589be7f08e73901e0e37d60
Roshini08 <151143452+Roshini08@users.noreply.github.com>
2025-06-22 21:20:40 +0530

    Create ques8.java

---
+++ 22-6-2025/ques8.java
@@ -0,0 +1,13 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 2, 9, 1};
+        int key = 9, index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                index = i;
+                break;
+            }
+        }
+        System.out.println(index != -1 ? "Found at index: " + index : "Not found");
+    }
+}
