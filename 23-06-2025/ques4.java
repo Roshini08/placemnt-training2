224c077f83bd1baf5f66d25120689098023b5a26
Roshini08 <151143452+Roshini08@users.noreply.github.com>
2025-06-23 19:08:10 +0530

    Create ques4.java

---
+++ 23-06-2025/ques4.java
@@ -0,0 +1,21 @@
+import java.util.LinkedHashMap;
+
+public class FirstNonRepeatingChar {
+    public static void main(String[] args) {
+        String str = "swiss";
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+
+        for (char c : str.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : map.keySet()) {
+            if (map.get(c) == 1) {
+                System.out.println("First non-repeating: " + c);
+                return;
+            }
+        }
+
+        System.out.println("No non-repeating character found.");
+    }
+}
